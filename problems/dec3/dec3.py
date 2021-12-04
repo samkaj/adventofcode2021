adb88a4c5feea338918340ffc44bf8805e67ed51
Samuel Kajava <kajavasamuel@gmail.com>
2021-12-04 11:32:44 +0100

    formatting and spelling

--- problems/dec3/dec3.py
+++ problems/dec3/dec3.py
@@ -3,35 +3,37 @@ def main():
     with open("problems/dec3/input.txt") as f:
         diagnostics = [line.strip() for line in f]
 
-    print(f'Energy consumtion: {power_consumtion(diagnostics)}')
-    print(f'Life support rating: {life_support_rating(diagnostics)}')
+    print(f"Energy consumtion: {power_consumption(diagnostics)}")
+    print(f"Life support rating: {life_support_rating(diagnostics)}")
 
 
-def power_consumtion(data) -> int:
-    bin_gamma = ''
-    bin_epsilon = ''
+def power_consumption(data) -> int:
+    bin_gamma = ""
+    bin_epsilon = ""
 
     for i in range(len(str(data[0]))):
         if sum_col(data, i) > len(data) / 2:
             # there are more ones than zeroes
-            bin_gamma += '1'
-            bin_epsilon += '0'
+            bin_gamma += "1"
+            bin_epsilon += "0"
         else:
             # there are less ones that zeroes
-            bin_epsilon += '1'
-            bin_gamma += '0'
+            bin_epsilon += "1"
+            bin_gamma += "0"
 
     return int(bin_gamma, 2) * int(bin_epsilon, 2)
+
 
 def life_support_rating(data):
     oxygen = rating(data, 0, True)
     co2 = rating(data, 0, False)
     return oxygen * co2
 
-def rating(data, position, most_common):  
-    # base case  
+
+def rating(data, position, most_common):
+    # base case
     if len(data) <= 1:
-        return int(data[0],2)
+        return int(data[0], 2)
 
     # find the most common bit
     criteria = 0
@@ -55,6 +57,7 @@ def rating(data, position, most_common):
 
 def sum_col(data, n):
     return sum([int(x[n]) for x in data])
+
 
 if __name__ == "__main__":
     main()
