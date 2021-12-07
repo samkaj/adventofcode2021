b9d94d447fb8a4a2f5defbf603d34d5260209032
Samuel Kajava <kajavasamuel@gmail.com>
2021-12-07 21:46:43 +0100

    add puzzle 2

--- problems/dec7/dec7.py
+++ problems/dec7/dec7.py
@@ -1,24 +1,38 @@
+from math import floor
+
+
 def main():
     # get input
     with open("problems/dec7/input.txt") as f:
         starting_positions = [int(x) for x in f.readline().split(",")]
 
     starting_positions.sort()
-    mid = int(len(starting_positions) / 2)
-    # Get from minimum median if there are two
-    fuel = min(
-        get_fuel_cost(starting_positions, starting_positions[mid]),
-        get_fuel_cost(starting_positions, starting_positions[mid - 1]),
-    )
-    print(f"In total: {fuel}")
+
+    fuel = get_fuel_cost_from_median(starting_positions)
+    print(f"In total (part 1): {fuel}")
+    fuel = get_fuel_cost_from_avg(starting_positions)
+    print(f"In total (part 2): {fuel}")
 
 
-def get_fuel_cost(positions, pos):
-    fuel = 0
+def get_fuel_cost_from_median(positions):
+    median_pos = positions[int(len(positions) / 2)]
+    total_fuel_cost = 0
     for crab in positions:
-        cost = abs(crab - pos)
-        fuel += cost
-    return fuel
+        # cost = total distance to the median value
+        cost = abs(crab - median_pos)
+        total_fuel_cost += cost
+    return total_fuel_cost
+
+
+def get_fuel_cost_from_avg(positions):
+    avg_pos = floor(sum(positions) / len(positions))
+    total_fuel_cost = 0
+    for crab in positions:
+        distance = abs(crab - avg_pos)
+        # cost = {1 + 2 + ... + (distance - 1) + (distance)}
+        cost = int(distance * (1 + distance) / 2)
+        total_fuel_cost += cost
+    return total_fuel_cost
 
 
 if __name__ == "__main__":
