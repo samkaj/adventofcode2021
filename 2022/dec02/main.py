7864f924439aec48de71e84743fe749241fd404b
samkaj <kajavasamuel@gmail.com>
2022-12-02 23:47:54 +0100

    Day two, part 2

--- 2022/dec02/main.py
+++ 2022/dec02/main.py
@@ -14,10 +14,25 @@ def part_one(rounds):
     }
     print(sum([outcomes[x] for x in rounds]))
 
+def part_two(rounds):
+    outcomes = {
+        'AX': 3,
+        'BX': 1,
+        'CX': 2,
+        'AY': 4,
+        'BY': 5,
+        'CY': 6,
+        'AZ': 8,
+        'BZ': 9,
+        'CZ': 7
+    }
+    print(sum([outcomes[x] for x in rounds]))
+
 def main():
     with open('input.txt', 'r') as f:
         rounds = [line.replace(' ','').strip() for line in f.readlines()]
     part_one(rounds)
+    part_two(rounds)
 
 
 if __name__ == '__main__':
