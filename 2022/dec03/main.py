4ab910f249501a359ee45a1e855b1a9d1dcbf05f
samkaj <kajavasamuel@gmail.com>
2022-12-04 22:02:14 +0100

    Horrible complexity I am sorry

--- 2022/dec03/main.py
+++ 2022/dec03/main.py
@@ -28,12 +28,30 @@ def get_dupe_quad(line: str) -> str:
             return c
     return ''
 
+def get_group(lines: list()) -> int:
+    '''
+    Part two.
+    I'm tired and hungover, we'll make it O(N^3)...
+    '''
+    for line in lines:
+        line = line[:-2] # remove newline
+        for c in line:
+            if c in lines[1] and c in lines[2]:
+                return get_prio(c)
+    return 0
+
 def main():
-    score = 0
+    score1 = 0
+    score2 = 0
     with open('input.txt', 'r') as f:
-        for line in f.readlines():
-            score += get_prio(get_dupe_quad(line))
-    print(score)
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            score1 += get_prio(get_dupe_quad(line))
+            if i % 3 == 0:
+                score2 += get_group(lines[i:i+3])
+                
+    print(f'Part one: {score1}')
+    print(f'Part two: {score2}')
 
 if __name__ == '__main__':
     main()
